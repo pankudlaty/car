dff6e820ae3cd4dbf77f599ffedded62fd671117
pankudlaty <pkownacki90@gmail.com>
2024-01-24 14:52:12 +0100

    visualizer in progress

--- visualizer.js
+++ visualizer.js
@@ -10,15 +10,50 @@ class Visualizer {
   }
   static drawLevel(ctx, level, left, top, width, height) {
     const right = left + width
-
     const bottom = top + height
+    const { inputs, outputs, weights, biases } = level
     const nodeRadius = 18
-    for (let i = 0; i < level.inputs.lenght; i++) {
-      const x = lerp(left, right, level.inputs.length == 1 ? 0.5 : i / (level.inputs.length - 1))
+    for (let i = 0; i < inputs.length; i++) {
+      for (let j = 0; j < inputs.length; j++) {
+        ctx.beginPath()
+        ctx.moveTo(
+          Visualizer.#getNodeX(inputs, i, left, right), bottom
+        )
+        ctx.lineTo(
+          Visualizer.#getNodeX(outputs, j, left, right), top
+        )
+        ctx.lineWidth = 2
+
+        ctx.strokeStyle = getRGBA(weights[i][j])
+        ctx.stroke()
+      }
+    }
+    for (let i = 0; i < inputs.lenght; i++) {
+      const x = Visualizer.#getNodeX(inputs, i, left, right)
       ctx.beginPath()
+      console.log(bottom)
       ctx.arc(x, bottom, nodeRadius, 0, Math.PI * 2)
-      ctx.fillStyle = "white"
+      ctx.fillStyle = "red"
       ctx.fill()
     }
+    for (let i = 0; i < outputs.lenght; i++) {
+      const x = Visualizer.#getNodeX(outputs, i, left, right)
+      ctx.beginPath()
+      ctx.arc(x, bottom, nodeRadius, 0, Math.PI * 2)
+      ctx.fillStyle = "red"
+      ctx.fill()
+      ctx.beginPath()
+      ctx.lineWidth = 2
+      ctx.arc(x, top, nodeRadius, 0, Math.PI * 2)
+      ctx.strokeStyle = getRGBA(biases[i])
+      ctx.stroke()
+    }
+  }
+  static #getNodeX(nodes, index, left, right) {
+    return lerp(
+      left,
+      right,
+      nodes.length == 1 ? 0.5 : index / (nodes.length - 1)
+    )
   }
 }
