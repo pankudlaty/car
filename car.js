30a752777e5977afd60cf6240dc10c55b592a3ea
pankudlaty <pkownacki90@gmail.com>
2024-01-26 15:17:57 +0100

    training time

--- car.js
+++ car.js
@@ -109,7 +109,7 @@ class Car {
     this.x -= Math.sin(this.angle) * this.speed
     this.y -= Math.cos(this.angle) * this.speed
   }
-  draw(ctx, color) {
+  draw(ctx, color, drawSensor = false) {
     if (this.damaged) {
       ctx.fillStyle = "gray"
     } else {
@@ -121,7 +121,7 @@ class Car {
       ctx.lineTo(this.polygon[i].x, this.polygon[i].y)
     }
     ctx.fill()
-    if (this.sensor) {
+    if (this.sensor && drawSensor) {
       this.sensor.draw(ctx)
     }
   }
